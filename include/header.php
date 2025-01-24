b2be5b98081a23c6bb9309ce66ffa69c78ab0b2b
Arbind-69 <arbinddash01@gmail.com>
2025-01-23 22:43:30 -0700

    dreamhousing

---
+++ include/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="nav-bar">
+                <div class="logo">Dream <br><span>Housing</span></div>
+
+                <div class="nav-links">
+                    <ul>
+                        <li><a href="./index.php"><span>Home</span></a></li>
+                        <li><a href="./about.php">About us</a></li>
+                        <li><a href="./services.php">Services</a></li>
+                        <li><a href="./projects.php">Projects</a></li>
+                        <li><a href="./contact.php">Contact us</a></li>
+                    </ul>
+                </div>
+
+
+
+
+            </div>
+        </div>
+
+    </header>
---
+++ index.php
@@ -0,0 +1,267 @@
+<?php
+include "./include/header.php";
+?>
+<div class="banner">
+    <div class="container">
+        <div class="banner-row">
+            <div class="banner-content">
+                <h1>
+                    <span>Building</span> Dreams,<br>Creating <span>Futures</span>
+                </h1>
+                <p>
+                    Delivery exception construction services with precision<br> and dedication, ensuring every project stands the test of time.
+                </p>
+                <div class="button">
+                    <div class="btn-1">
+                        <a href="">Explore More</a>
+
+                    </div>
+                    <div class="btn-2">
+                        <a href="">Contact us</a>
+
+                    </div>
+
+
+                </div>
+
+
+
+
+            </div>
+            <div class="banner-image">
+                <img src="./img/img2.png" alt="">
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+
+<div class="total-info">
+    <div class="container">
+
+        <div class="total-number">
+            <div class="number">
+                <div class="info-nmb">
+                    <h1>
+                        100+
+                    </h1>
+                    <p>
+                        Project <br>completed
+                    </p>
+
+                </div>
+                <div class="info-nmb">
+                    <h1>
+                        226
+                    </h1>
+                    <p>
+                        total <br>Employees
+                    </p>
+
+                </div>
+
+                <div class="info-nmb">
+                    <h1>
+                        100%
+                    </h1>
+                    <p>
+                        satisfied <br>Clients
+                    </p>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+        <div class="paragraph">
+            <div class="info-page">
+                <h1>
+                    Why Choose Us?
+                </h1>
+                <p>
+                    Bastu Homes Provides Services With Years Of experience in the construction industry. our skilled team brings unparalleled expertise and knowledge to every project.
+                    we are committed to delivering top-notch quality and innovative tailored to meet your unique needs.
+                </p>
+
+
+            </div>
+
+            <div class="info-boxes">
+                <div class="single-box">
+                    
+                        <h1>
+                            .1
+                        </h1>
+                        
+                    
+                    <div class="head-page">
+                        <h3>
+                            Quality Craftsmanship
+                        </h3>
+                        <p>
+
+                        We Pride ourselves on our meticlous attention to detail and dedication to superior craftsmanship.
+                        </p>
+
+                    </div>
+                   
+                   
+
+
+                </div>
+                <div class="single-box">
+                    
+                        <h1>
+                            .2
+                        </h1>
+                        
+                    
+                    <div class="head-page">
+                        <h3>
+                            Customer-centric Approachs
+                        </h3>
+                        <p>
+
+                        Your satisfaction is our top priority. we work closely with you throughout the entire construction.
+                        </p>
+
+                    </div>
+                   
+                   
+
+
+                </div>
+                <div class="single-box">
+                    
+                        <h1>
+                            .3
+                        </h1>
+                        
+                    
+                    <div class="head-page">
+                        <h3>
+                            Rellability and integrity
+                        </h3>
+                        <p>
+
+                        From concept to completion, we handle every aspect of your project with a full range of services.
+                        </p>
+
+                    </div>
+                   
+                   
+
+
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+<div class="our-services">
+    <div class="container">
+        <div class="services-title">
+            <div class="services-head">
+                <h1>
+                    Get a Quote For your project
+                </h1>
+                <div class="services-tag">
+                <a href="">free quote</a>
+                </div>
+                
+                
+
+            </div>
+
+        </div>
+
+        <div class="services-nmb">
+            <div class="services-page">
+                <div class="box">
+                    <div class="box-icon">
+                    <i class="fa-solid fa-user-nurse"></i>
+                    </div>
+                    <h2>
+                        service 1
+                    </h2>
+                    
+                    
+                </div>
+                <div class="box">
+                    <div class="box-icon">
+                    <i class="fa-solid fa-user-nurse"></i>
+                    </div>
+                    <h2>
+                        service 2
+                    </h2>
+                    
+                    
+                </div>
+                <div class="box">
+                    <div class="box-icon">
+                    <i class="fa-solid fa-user-nurse"></i>
+                    </div>
+                    <h2>
+                        service 3
+                    </h2>
+                    
+                    
+                </div>
+                <div class="box">
+                    <div class="box-icon">
+                    <i class="fa-solid fa-user-nurse"></i>
+                    </div>
+                    <h2>
+                        service 4
+                    </h2>
+                    
+                    
+                </div>
+                <div class="box">
+                    <div class="box-icon">
+                    <i class="fa-solid fa-user-nurse"></i>
+                    </div>
+                    <h2>
+                        service 5
+                    </h2>
+                    
+                    
+                </div>
+                <div class="box">
+                    <div class="box-icon">
+                    <i class="fa-solid fa-user-nurse"></i>
+                    </div>
+                    <h2>
+                        service 5
+                    </h2>
+                    
+                    
+                </div>
+                
+                
+
+            </div>
+
+        </div>
+
+
+    </div>
+
+</div>
+
+
+
+
+
+
+
+
+<?php
+include "./include/footer.php";
+?>
